02016d49ae9b697a625997e4a272bef154057494
Henry Ellis <ellish@umich.edu>
2017-11-08 16:29:44 -0500

    testing out travis ci

--- rpn.py
+++ rpn.py
@@ -1,27 +1,37 @@
 #!/usr/bin/python
 
-def calculate(myarg): 
-	stack = list()
-	for token in myarg.split:
-		if token == '+':
-			arg1 = stack.pop();
-			arg2 = stack.pop();
-			result = arg1 + arg2
-			stack.append(result)
-		elif token == '-':
-			arg2 = stack.pop()
-			arg1 = stack.pop()
-			result = arg2 - arg1
-			stack.append(result)
-		else:
-			stack.append(int(token))
-	print (stack)
-	return stack.pop()
+import operator
+
+
+operators = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.truediv,
+}
+
+def calculate(myarg):
+    stack = list()
+    for token in myarg.split():
+        try:
+            token = int(token)
+            stack.append(token)
+        except ValueError:
+            function = operators[token]
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = function(arg1, arg2)
+            stack.append(result)
+        print(stack)
+    if len(stack) != 1:
+        raise TypeError("Too many parameters")
+    return stack.pop()
 
 def main():
-	a = calculate("1 1 +")
-	print (a)
+    while True:
+        result = calculate(input("rpn calc> "))
+        print("Result: ", result)
 
-if __name__ == "__main __":
-	main()
-	
+if __name__ == '__main__':
+    main()
+
